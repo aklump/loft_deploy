f494650bf1575604dc483500f82a76d938805475
aklump <sourcecode@intheloftstudios.com>
2015-08-03 12:28:50 -0700

    Version bumped from 0.11.1 to 0.11.2

--- loft_deploy.sh
+++ loft_deploy.sh
@@ -103,7 +103,7 @@ mysql_check_result=false
 now=$(date +"%Y%m%d_%H%M")
 
 # Current version of this script (auto-updated during build).
-ld_version=0.11.1
+ld_version=0.11.2
 
 # theme color definitions
 color_red=1
