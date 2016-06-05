c014a9d2fb7510fc8129a58749c37bddcc3e191c
aklump <sourcecode@intheloftstudios.com>
2016-06-05 08:34:26 -0700

    Added dynamic parsing of drupal settings for db credentials.

---
+++ includes/drupal_settings.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Read a drupal settings file dynamically pulling out the db credentials
+ */
+
+
+define('DRUPAL_ROOT', '/');
+
+function t($a) {
+  return $a;
+}
+
+$path_to_settings = $argv[1];
+$db_key = isset($argv[2]) ? $argv[2] : 'default';
+$fallback = 'default';
+
+try {
+
+  if (!is_readable($path_to_settings)) {
+    throw "$settings settings file is not readable.";
+  }
+
+  require $path_to_settings;
+
+  if (!isset($databases[$db_key][$fallback])) {
+    throw 'Missing $database variable.';
+  }
+  $db = $databases[$db_key][$fallback];
+
+  if ($db['driver'] !== 'mysql') {
+    throw 'Drivers other than mysql are not yet supported by loft_deploy';
+  }
+
+  $return[] = empty($db['host']) ? 'localhost' : $db['host'];
+  $return[] = $db['database'];
+  $return[] = $db['username'];
+  $return[] = $db['password'];
+
+} catch (Exception $e) {
+  $return = array_fill(0, 4, '?');
+}
+echo implode(' ', $return);
