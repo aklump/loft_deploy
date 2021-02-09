beb8771769ba461d4ce66696451417534a7942a6
Aaron Klump <sourcecode@intheloftstudios.com>
2021-02-09 14:48:11 -0800

    apply code formatting.

--- loft_deploy.sh
+++ loft_deploy.sh
@@ -57,7 +57,7 @@
 #
 
 # Define the configuration file relative to this script.
-CONFIG="loft_deploy.yml";
+CONFIG="loft_deploy.yml"
 
 # Uncomment this line to enable file logging.
 #LOGFILE="loft_deploy.log"
@@ -69,8 +69,8 @@ function on_pre_config() {
 }
 
 function on_compile_config() {
-    # Make the instance configuration accessible to Cloudy.
-    [ -f "$config_dir/config.yml" ] && echo "$config_dir/config.yml"
+  # Make the instance configuration accessible to Cloudy.
+  [ -f "$config_dir/config.yml" ] && echo "$config_dir/config.yml"
 }
 
 function on_clear_cache() {
@@ -105,18 +105,18 @@ source "$r/cloudy/cloudy.sh"
 # End Cloudy Bootstrap
 
 function get_version() {
-    local version=$(grep "\"version\": \"" "$ROOT/composer.json")
-    version=${version/\"version\": \"/}
-    version=${version/\",/}
-    echo $version
+  local version=$(grep "\"version\": \"" "$ROOT/composer.json")
+  version=${version/\"version\": \"/}
+  version=${version/\",/}
+  echo $version
 }
 
 # Input validation.
 validate_input || exit_with_failure "Input validation failed."
 
 ##
- # Bootstrap
- #
+# Bootstrap
+#
 eval $(get_config_as "ld_mysql" "bin.mysql" "/usr/bin/mysql")
 eval $(get_config_as "ld_mysqldump" "bin.mysqldump" "/usr/bin/mysqldump")
 eval $(get_config_as "ld_gzip" "bin.gzip" "/usr/bin/gzip")
@@ -147,8 +147,8 @@ for arg in "$@"; do
   fi
 done
 ##
- # End Bootstrap
- #
+# End Bootstrap
+#
 
 # The user's operation
 op=$(get_command)
@@ -181,20 +181,19 @@ ld_remote_rsync_cmd="rsync -azP"
 has_option "v" && ld_remote_rsync_cmd="rsync -azPv"
 
 ##
- # Begin Controller
- #
+# Begin Controller
+#
 
 # Input validation.
 validate_input || exit_with_failure "Unknown command."
 implement_cloudy_basic
-
 
 # init has to come before configuration loading
 if [ "$op" == 'init' ]; then
   if _access_check $op; then
     init $2
   else
-    echo "`tty -s && tput setaf 1`ACCESS DENIED!`tty -s && tput op`"
+    echo "$(tty -s && tput setaf 1)ACCESS DENIED!$(tty -s && tput op)"
     end "$local_role sites may not invoke: loft_deploy $op"
   fi
 fi
@@ -205,21 +204,21 @@ if [ "$op" == 'update' ]; then
   if _access_check $op; then
     update $2
   else
-    echo "`tty -s && tput setaf 1`ACCESS DENIED!`tty -s && tput op`"
+    echo "$(tty -s && tput setaf 1)ACCESS DENIED!$(tty -s && tput op)"
     end "$local_role sites may not invoke: loft_deploy $op"
   fi
 fi
 
 ##
- # Access Check
- #
+# Access Check
+#
 if ! _access_check $op; then
-    exit_with_failure "The \"$op\" command is not allowed for \"$local_role\" role environments."
+  exit_with_failure "The \"$op\" command is not allowed for \"$local_role\" role environments."
 fi
 
 ##
- # Call the correct handler
- #
+# Call the correct handler
+#
 
 # Determine the server being operated on
 source_server='prod'
@@ -232,10 +231,9 @@ if [ $op == "get" ]; then
   exit 1
 fi
 
-
 if [[ "$(get_migration_type)" != "push" ]]; then
-    print_header
-    auto_update
+  print_header
+  auto_update
 fi
 
 #
@@ -245,197 +243,197 @@ status=true
 handle_pre_hook $op || status=false
 
 case $op in
-  'config')
-      [[ "$EDITOR" ]] && $EDITOR "$config_dir/config.yml" && exit_with_cache_clear
-      exit_with_failure "You must set environment variable \$EDITOR with a command first, e.g. in ~/.bash_profile, export EDITOR=nano"
-    ;;
+'config')
+  [[ "$EDITOR" ]] && $EDITOR "$config_dir/config.yml" && exit_with_cache_clear
+  exit_with_failure "You must set environment variable \$EDITOR with a command first, e.g. in ~/.bash_profile, export EDITOR=nano"
+  ;;
 
-  'migrate')
-    [[ "$status" == true ]] && do_migrate || status=false
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && exit_with_success_elapsed "Migration complete"
-    exit_with_failure "Migration failed."
-    ;;
-  'init')
-    [[ "$status" == true ]] && init ${SCRIPT_ARGS[1]} || status=false
-    handle_post_hook $op $status && exit 0
-    exit 1
-    ;;
+'migrate')
+  [[ "$status" == true ]] && do_migrate || status=false
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && exit_with_success_elapsed "Migration complete"
+  exit_with_failure "Migration failed."
+  ;;
+'init')
+  [[ "$status" == true ]] && init ${SCRIPT_ARGS[1]} || status=false
+  handle_post_hook $op $status && exit 0
+  exit 1
+  ;;
 
-  'configtest')
-    [[ "$status" == true ]] && configtest || status=false
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && exit_with_success_elapsed 'Test complete.'
-    exit_with_failure 'Test complete with failure(s).'
-    ;;
+'configtest')
+  [[ "$status" == true ]] && configtest || status=false
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && exit_with_success_elapsed 'Test complete.'
+  exit_with_failure 'Test complete with failure(s).'
+  ;;
 
-  'import')
-    [[ "$status" == true ]] && import_db ${SCRIPT_ARGS[1]} || status=false
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && exit_with_success_elapsed "Import complete."
-    exit_with_failure "Import failed."
-    ;;
+'import')
+  [[ "$status" == true ]] && import_db ${SCRIPT_ARGS[1]} || status=false
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && exit_with_success_elapsed "Import complete."
+  exit_with_failure "Import failed."
+  ;;
 
-  'export')
-    suffix=${SCRIPT_ARGS[1]}
-    has_option "time" && suffix="${suffix}-$(date8601 -c)"
-    [[ "$status" == true ]] && export_db ${suffix#-} || status=false
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && exit_with_success_elapsed 'Export complete.'
-    exit_with_failure 'Export failed.'
-    ;;
+'export')
+  suffix=${SCRIPT_ARGS[1]}
+  has_option "time" && suffix="${suffix}-$(date8601 -c)"
+  [[ "$status" == true ]] && export_db ${suffix#-} || status=false
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && exit_with_success_elapsed 'Export complete.'
+  exit_with_failure 'Export failed.'
+  ;;
 
-  'export-purge')
-    _current_db_paths
+'export-purge')
+  _current_db_paths
 
-    if [ ! -d "$current_db_dir" ]; then
-      fail_because "$current_db_dir is not a directory" && exit_with_failure
-    fi
+  if [ ! -d "$current_db_dir" ]; then
+    fail_because "$current_db_dir is not a directory" && exit_with_failure
+  fi
 
-    days=$(get_command_arg "days")
-    paths=($(find $current_db_dir/*.sql.gz -mtime +$days))
-    paths=("${paths[@]}" $(find $current_db_dir/*.sql -mtime +$days))
-    echo_heading "Purge Export Files"
-    table_clear
-    table_add_row "Directory" "$current_db_dir"
-    table_add_row "Age in Days" "$days"
-    table_add_row "Total Found" "${#paths[@]}"
-    echo_table
+  days=$(get_command_arg "days")
+  paths=($(find $current_db_dir/*.sql.gz -mtime +$days))
+  paths=("${paths[@]}" $(find $current_db_dir/*.sql -mtime +$days))
+  echo_heading "Purge Export Files"
+  table_clear
+  table_add_row "Directory" "$current_db_dir"
+  table_add_row "Age in Days" "$days"
+  table_add_row "Total Found" "${#paths[@]}"
+  echo_table
 
-    list_clear
-    for path in "${paths[@]}"; do
-      if has_option "dry-run"; then
-        list_add_item "$(basename $path)"
-      else
-        if has_option "yes" || confirm "$LI $(basename $path) $(echo_red "Delete?")"; then
-          if rm "$path"; then
-            succeed_because "$(basename $path) deleted."
-          else
-            fail_because "Could not delete $path"
-          fi
-        else
-          echo_yellow_highlight "Skipped."
-        fi
-      fi
-    done
-
+  list_clear
+  for path in "${paths[@]}"; do
     if has_option "dry-run"; then
-      echo_list
-    fi
-
-    has_failed && exit_with_failure
-    exit_with_success
-    ;;
-
-  'fetch')
-    suffix=''
-    status=true
-    if [[ "$source_server" != 'prod' ]]; then
-      suffix=" --$source_server"
-    fi
-    if [[ "$status" == true ]] && has_asset database; then
-      fetch_db || status=false
-    fi
-    if [[ "$status" == true ]] && has_asset files; then
-      fetch_files || status=false
-    fi
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && complete_elapsed "Fetch complete." && exit 0
-    exit_with_failure "Fetch failed."
-    ;;
-
-  'reset')
-    if [[ "$status" == true ]] && has_asset database; then
-        reset_db && echo "Local database has been reset to match $source_server." || status=false
-    fi
-    if [[ "$status" == true ]] && has_asset files; then
-        reset_files && echo "Local files have been reset to match $source_server." || status=false
-    fi
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && complete_elapsed "Reset complete." && exit 0
-    exit_with_failure "Reset failed."
-    ;;
-
-  'pull')
-    [[ "$status" == true ]] && do_pull || status=false
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && complete_elapsed "Pull complete." && exit 0
-    exit_with_failure "Pull failed."
-    ;;
-
-  'push')
-    if [[ "$status" == true ]] && has_asset database; then
-      push_db || status=false
-    fi
-    if [[ "$status" == true ]] && has_asset files; then
-      push_files || status=false
-    fi
-
-    handle_post_hook $op $status || status=false
-    [[ "$status" == true ]] && complete_elapsed "Push complete." && exit 0
-    exit_with_failure "Push failed."
-    ;;
-
-  'hook')
-    if [ "${SCRIPT_ARGS[1]}" ]; then
-      handle_pre_hook "${SCRIPT_ARGS[1]}" $status || status=false
-      handle_post_hook "${SCRIPT_ARGS[1]}" $status || status=false
+      list_add_item "$(basename $path)"
     else
-        echo_red "What type of hook? e.g. ldp hook reset"
+      if has_option "yes" || confirm "$LI $(basename $path) $(echo_red "Delete?")"; then
+        if rm "$path"; then
+          succeed_because "$(basename $path) deleted."
+        else
+          fail_because "Could not delete $path"
+        fi
+      else
+        echo_yellow_highlight "Skipped."
+      fi
     fi
-    end
-    ;;
+  done
 
-  'mysql')
-    loft_deploy_mysql "${SCRIPT_ARGS[1]}" || status=false
-    handle_post_hook $op || status=false
-    complete 'Your mysql session has ended.'
-    exit 0
-    ;;
+  if has_option "dry-run"; then
+    echo_list
+  fi
 
-  'ls')
-    if has_option d; then
-      do_ls "$local_db_dir"
-    fi
-    if has_option f; then
-      do_ls "$local_files"
-    fi
-    handle_post_hook $op
-    end
-    ;;
+  has_failed && exit_with_failure
+  exit_with_success
+  ;;
 
-  'info')
-    show_info || status=false
-    handle_post_hook $op $status && complete_elapsed "Info displayed" && exit 0
-    exit_with_failure
-    ;;
+'fetch')
+  suffix=''
+  status=true
+  if [[ "$source_server" != 'prod' ]]; then
+    suffix=" --$source_server"
+  fi
+  if [[ "$status" == true ]] && has_asset database; then
+    fetch_db || status=false
+  fi
+  if [[ "$status" == true ]] && has_asset files; then
+    fetch_files || status=false
+  fi
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && complete_elapsed "Fetch complete." && exit 0
+  exit_with_failure "Fetch failed."
+  ;;
 
-  'terminus')
-    cmd="auth:login --machine-token=$terminus_machine_token"
+'reset')
+  if [[ "$status" == true ]] && has_asset database; then
+    reset_db && echo "Local database has been reset to match $source_server." || status=false
+  fi
+  if [[ "$status" == true ]] && has_asset files; then
+    reset_files && echo "Local files have been reset to match $source_server." || status=false
+  fi
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && complete_elapsed "Reset complete." && exit 0
+  exit_with_failure "Reset failed."
+  ;;
 
-    # Todo need to array_shift and then pass the entire bit to terminus to make
-    # this really work correctly.
-    if [[ "${SCRIPT_ARGS[1]}" ]]; then
-      cmd="${SCRIPT_ARGS[1]}"
-    fi
-    $ld_terminus $cmd && echo_green "└── $config_dir/vendor/bin/terminus" || exit_with_failure
-    end
-    ;;
+'pull')
+  [[ "$status" == true ]] && do_pull || status=false
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && complete_elapsed "Pull complete." && exit 0
+  exit_with_failure "Pull failed."
+  ;;
 
-  'config-export')
-    echo_heading "Copy and paste the following:"
-    echo
-    if [[ "$local_role" == "prod" ]]; then
-      echo "production:"
-    elif [[ "$local_role" == "staging" ]]; then
-      echo "staging:"
-    fi
-    echo "  config: $config_dir"
-    echo "  script: $0"
-    echo "  user: $(whoami)"
-    echo "  host: $(get_public_ip)"
-    exit_with_success
-    ;;
+'push')
+  if [[ "$status" == true ]] && has_asset database; then
+    push_db || status=false
+  fi
+  if [[ "$status" == true ]] && has_asset files; then
+    push_files || status=false
+  fi
+
+  handle_post_hook $op $status || status=false
+  [[ "$status" == true ]] && complete_elapsed "Push complete." && exit 0
+  exit_with_failure "Push failed."
+  ;;
+
+'hook')
+  if [ "${SCRIPT_ARGS[1]}" ]; then
+    handle_pre_hook "${SCRIPT_ARGS[1]}" $status || status=false
+    handle_post_hook "${SCRIPT_ARGS[1]}" $status || status=false
+  else
+    echo_red "What type of hook? e.g. ldp hook reset"
+  fi
+  end
+  ;;
+
+'mysql')
+  loft_deploy_mysql "${SCRIPT_ARGS[1]}" || status=false
+  handle_post_hook $op || status=false
+  complete 'Your mysql session has ended.'
+  exit 0
+  ;;
+
+'ls')
+  if has_option d; then
+    do_ls "$local_db_dir"
+  fi
+  if has_option f; then
+    do_ls "$local_files"
+  fi
+  handle_post_hook $op
+  end
+  ;;
+
+'info')
+  show_info || status=false
+  handle_post_hook $op $status && complete_elapsed "Info displayed" && exit 0
+  exit_with_failure
+  ;;
+
+'terminus')
+  cmd="auth:login --machine-token=$terminus_machine_token"
+
+  # Todo need to array_shift and then pass the entire bit to terminus to make
+  # this really work correctly.
+  if [[ "${SCRIPT_ARGS[1]}" ]]; then
+    cmd="${SCRIPT_ARGS[1]}"
+  fi
+  $ld_terminus $cmd && echo_green "└── $config_dir/vendor/bin/terminus" || exit_with_failure
+  end
+  ;;
+
+'config-export')
+  echo_heading "Copy and paste the following:"
+  echo
+  if [[ "$local_role" == "prod" ]]; then
+    echo "production:"
+  elif [[ "$local_role" == "staging" ]]; then
+    echo "staging:"
+  fi
+  echo "  config: $config_dir"
+  echo "  script: $0"
+  echo "  user: $(whoami)"
+  echo "  host: $(get_public_ip)"
+  exit_with_success
+  ;;
 
 esac
 
