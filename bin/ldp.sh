d8a3672fc15ad3d31600e3c5f004ed6112257d82
Aaron Klump <sourcecode@intheloftstudios.com>
2021-10-08 13:13:01 -0700

    Add live_dev_porter support to ldp.sh

--- bin/ldp.sh
+++ bin/ldp.sh
@@ -7,14 +7,24 @@
 # See documentation for installation/usage.
 #
 
+function find_live_dev_porter() {
+  test / == "$PWD" && return 1
+  test -e "./bin/live_dev_porter" && echo "${PWD}/bin/live_dev_porter" && return 0 || cd .. && find_live_dev_porter
+}
 function find_loft_deploy() {
   test / == "$PWD" && return 1
   test -e "./vendor/bin/loft_deploy.sh" && echo "${PWD}/vendor/bin/loft_deploy.sh" && return 0 || cd .. && find_loft_deploy
 }
 
-LOFT_DEPLOY=$(find_loft_deploy)
-if [[ ! "$LOFT_DEPLOY" ]]; then
-  echo "loft_deploy.sh not found. Have you installed it? [composer require aklump/loft-deploy]" && exit 1
-fi
+# This is the successor to loft_deploy, so support it if found.
+LIVE_DEV_PORTER=$(find_live_dev_porter)
+if [[ "$LIVE_DEV_PORTER" ]]; then
+  . "$LIVE_DEV_PORTER" "$@"
+else
+  LOFT_DEPLOY=$(find_loft_deploy)
+  if [[ ! "$LOFT_DEPLOY" ]]; then
+    echo "loft_deploy.sh not found. Have you installed it? [composer require aklump/loft-deploy]" && exit 1
+  fi
 
-. "$LOFT_DEPLOY" "$@"
+  . "$LOFT_DEPLOY" "$@"
+fi
