5efdc6c8bfe98499ba54ebea11e69a0add440d04
Aaron Klump <sourcecode@intheloftstudios.com>
2022-02-15 13:11:10 -0800

    Add "old_%" to no data example file.

--- init/base/sql/db_tables_no_data.sql
+++ init/base/sql/db_tables_no_data.sql
@@ -1,1 +1,1 @@
-SELECT table_name FROM information_schema.tables WHERE table_schema = '$local_db_name' AND table_name LIKE 'cache%';
+SELECT table_name FROM information_schema.tables WHERE table_schema = '$local_db_name' AND (table_name LIKE 'cache%' OR table_name LIKE 'old_%');
