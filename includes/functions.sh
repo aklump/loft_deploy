e745c5be12debf184bfc8d012a074037a3f505dd
Aaron Klump <sourcecode@intheloftstudios.com>
2022-05-02 21:11:03 -0700

    suppress ionice not found message.

--- includes/functions.sh
+++ includes/functions.sh
@@ -2457,7 +2457,7 @@ function ssh_staging() {
 #
 # Returns nothing
 function _in_the_background {
-  $(which ionice) || return
+  which ionice >/dev/null || return
   ionice -c 2 -n 7 -p $BASHPID >/dev/null
   renice  +10 -p  $BASHPID >/dev/null
 }
